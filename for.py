c793da55f4bff0578e6c283542e2199db5669332
Askar Batyrbekov <altynadam123@gmail.com>
2023-02-18 16:10:54 +0600

    test commit

---
+++ for.py
@@ -0,0 +1,18 @@
+int1 = '13 212 543'.split()
+int2 = '13_212_553'
+
+# list comprehension
+
+intet = 0
+t = [i for i in range(len(int2))]
+#for i in range(len(int2))
+#    intet += 1
+
+
+
+
+#t = []
+#for i in range(len(int2))
+#   t.append(i)
+#
+print(t)
---
+++ zada4i.py
@@ -0,0 +1,68 @@
+#for i in range(1, 1000):
+#    if i % 2 == 0:
+#        if i % 7 == 0:
+#            print(i)
+
+#c = 'abcdefjhijklmnopqrstuvwxyz'
+#for b in range(len(c)):
+#    print(b, c[b])
+
+
+#health = 1000
+#sport = 100
+#lessons = 180
+#hw = 60
+#cry = 200
+#music = 60
+#eat = 150
+#sleep = 500
+
+
+#while health > 0:
+ #   action = input("choose action: ")
+  #  if action == 'sleep':
+   #     health += sleep
+    #    print(f"Health: {health}")
+    #if action == 'lessons':
+     #   health -= lessons
+     #   print(f"Health: {health}")
+   # if action == 'sport':
+    #    health -= sport
+    #    print(f"Health: { health}")
+   # if action == 'hw':
+ #       health -= hw
+  #      print(f"Health: {health}")
+   # if action == 'cry':
+   #     health -= cry
+    #    print(f"Health: {health}")
+    #if action == 'music':
+     #   health += music
+      #  print(f"Health: {health}")
+  #  if action == 'eat':
+   #     health += eat
+    #    print(f"Health: {health}")
+#else:
+#   print(f'Fatality! Health: {health}')
+
+
+
+
+
+incorrect_symbols = "!@#$%^&*()_+"
+password1 = 'AAgwegwerg1541' #input("введите пароль:")
+# password 2 = input ("подтвердите пароль: ")
+digits = '0987654321ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+invalid = '3'
+while True:
+    for i in password1:
+        if i in incorrect_symbols:
+            print('некорректные символы!')
+            break
+        else:
+    if password1 == password2:
+        if len(password1) == len(isvalid):
+
+
+
+
+
